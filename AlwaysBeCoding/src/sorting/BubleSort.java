35724c45c939464bb625b0eee1de39b6ab76faa2
HuyHateChii <huytv.cntt@gmail.com>
2018-04-13 21:54:32 +0700

    Bubble sort

---
+++ AlwaysBeCoding/src/sorting/BubleSort.java
@@ -0,0 +1,40 @@
+package sorting;
+import java.util.Random;
+
+public class BubleSort {
+
+	public static void main(String[] args) {
+		int arr[] = new int[5];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = new Random().nextInt(10);
+		}
+
+		System.out.print("before sort: ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		int sortedArr[] = sortByBubble(arr);
+
+		System.out.println(" ");
+
+		System.out.print("after sort: ");
+		for (int i = 0; i < sortedArr.length; i++) {
+			System.out.print(sortedArr[i] + " ");
+		}
+		// 1 2 3 4 5
+		// 1 2 3 4 5
+	}
+
+	private static int[] sortByBubble(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] < arr[j + 1]) {
+					int temp = arr[j + 1];
+					arr[j + 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+}
